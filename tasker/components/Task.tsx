e5a78dedfb87c54d18787822f825c19ba794bfd3
Karan Bhullar <cswailord@gmail.com>
2024-07-16 00:58:58 -0400

    add base urgency

--- tasker/components/Task.tsx
+++ tasker/components/Task.tsx
@@ -1,6 +1,14 @@
 import { Text } from "react-native-reanimated/lib/typescript/Animated";
+import { Urgency } from "@/constants/Urgency";
+type taskProps = {
+    taskName: string,
+    category : string | boolean,
+    urgency? : number,
+    deadline? : Date
+}
+// finish later
 
-function Task() {
+function Task(props : taskProps) {
     <Text>Testing one two three</Text>
 };
 
---
+++ tasker/constants/Urgency.ts
@@ -0,0 +1,8 @@
+export const Urgency = {
+    0: {
+        color : "#AFE1AF"
+    },
+    1: {},
+    2: {}
+
+}
